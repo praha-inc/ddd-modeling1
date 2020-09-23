966c2ec20631812726c2b7a3232d1165cebc43ad
shunya matsubara <shun.fzs1000@gmail.com>
2020-09-23 19:14:05 +0900

    :+1: isAdmin追加

--- domain/clerk-user/entity/clerk-user.ts
+++ domain/clerk-user/entity/clerk-user.ts
@@ -2,10 +2,12 @@
   export class ClerkUser {
     public readonly id: string
     public readonly teamId: string
-    public constructor(params: { id: string; teamId: string }) {
-      const { id, teamId } = params
+    public readonly isAdmin: boolean // memo: 商用サービスなら「管理者」以外にも色々な権限が出るはずなのでvalueObjectにすると思うけど、今回は勉強用なのでisAdminだけにしておく
+    public constructor(params: { id: string; teamId: string; isAdmin: boolean }) {
+      const { id, teamId, isAdmin } = params
       this.id = id
       this.teamId = teamId
+      this.isAdmin = isAdmin
     }
     public isEqual(otherClerkUser: ClerkUser) {
       return otherClerkUser.id === this.id && otherClerkUser.teamId === this.teamId
