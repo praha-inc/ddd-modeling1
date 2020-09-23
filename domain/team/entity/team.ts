593272c0512d71ba4cbf9f2004d7ab41db98ebb2
harhogefoo <harimanmon@gmail.com>
2020-09-23 20:07:26 +0900

    feat: TeamPaymentを追加

--- domain/team/entity/team.ts
+++ domain/team/entity/team.ts
@@ -1,24 +1,41 @@
+class TeamPayment {
+  private readonly id: string
+  private readonly paymentRequiredDate: Date
+  private readonly paid: boolean
+
+  public constructor(params: { id: string; paymentRequiredDate: Date, paid: boolean}) {
+    const { id, paymentRequiredDate, paid } = params
+    this.id = id
+    this.paymentRequiredDate = paymentRequiredDate
+    this.paid = paid
+  }
+
+  public isPaid() {
+    return this.paid
+  }
+}
+
 export class Team {
   private readonly id: string
   private readonly clerkUserIds: string[]
+  private readonly teamPayments: TeamPayment[]
   private static limit = 1
-  private readonly paid: boolean
-  public constructor(params: { id: string; clerkUserIds: string[], paid: boolean }) {
-    const { id, clerkUserIds, paid } = params
+  public constructor(params: { id: string; clerkUserIds: string[], teamPayments: TeamPayment[] }) {
+    const { id, clerkUserIds, teamPayments } = params
 
     if (clerkUserIds.length < Team.limit) {
       throw new Error(`cannot create team with user count below: ${Team.limit}`)
     }
     this.id = id
     this.clerkUserIds = clerkUserIds
-    this.paid = paid
+    this.teamPayments = teamPayments
   }
   public isEqual(otherTeam: Team) {
     return otherTeam.id === this.id
   }
 
   public isPaid() {
-    return this.paid
+    return this.teamPayments.every(teamPayment => teamPayment.isPaid)
   }
 
   public isPaymentRequired() {
