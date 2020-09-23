5458477d47a0f36d639dc56372bc1bbb67900cc7
shunya matsubara <shun.fzs1000@gmail.com>
2020-09-23 19:43:28 +0900

    use connection in factory

--- infra/rootPostFactoryImpl.ts
+++ infra/rootPostFactoryImpl.ts
@@ -2,7 +2,15 @@ import { RootPost } from "../domain/root-post/entity/root-post";
 import { RootPostFactory } from "../domain/root-post/factory/rootPostFactory";
 import { Status } from "../domain/root-post/valueObject/status";
 
+type Connection = {
+};
+
 export class RootPostFactoryImpl implements RootPostFactory {
+  private readonly conn: Connection;
+  constructor(conn: Connection) {
+    this.conn = conn;
+  }
+
   public createRootPost(params: {
     id: string;
     content: string;
